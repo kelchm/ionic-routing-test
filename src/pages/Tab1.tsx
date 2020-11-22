6e57ca9f14f333e5de45d60231cb6ae523833429
Martin Foerster <martin@atroo.de>
2020-11-22 22:27:15 +0100

    outline basic test to illustrate routing

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonButton,
+} from "@ionic/react";
+import "./Tab1.css";
+import { useHistory, useParams, useLocation } from "react-router";
 
 const Tab1: React.FC = () => {
+  const history = useHistory();
+  const params: any = useParams();
+  const location = useLocation();
+
+  let routeState: any = location.state || {};
+  let counter = parseInt(routeState.counter || params.id, 10) || 1;
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +35,34 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        Current counter: {counter}
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText>
+                Route to the same Page via route state (does not trigger
+                rerender)
+              </IonText>
+              <IonButton
+                onClick={() =>
+                  history.push("/tab1", {
+                    counter: counter + 1,
+                  })
+                }
+              >
+                Route
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonText>
+                Route to the same Page via route param (triggers rerender)
+              </IonText>
+              <IonButton onClick={() => history.push("/tab1/" + (counter + 1))}>
+                Route
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
